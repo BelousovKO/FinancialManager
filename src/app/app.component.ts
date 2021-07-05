17e48862b4f53482f5c207d40fe6edadfc579627
belousovko <working.belousovko@gmail.com>
2021-07-05 23:06:01 +0700

    повторная попытка задиплоить

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthorizationService} from './services/authorization.service';
 import {UserDataService} from './services/user-data.service';
-import {DemoDataService} from './services/demo-data.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,6 @@ import {DemoDataService} from './services/demo-data.service';
 export class AppComponent {
 
   constructor(public authorization: AuthorizationService,
-              public userData: UserDataService,
-              public demoData: DemoDataService) {
+              public userData: UserDataService) {
   }
 }
